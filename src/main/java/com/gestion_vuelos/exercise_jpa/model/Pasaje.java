558071c54f7b6687acddb05c37c65211935b2067
Lautaro Soria <lsoria@baufest.com>
2025-08-01 18:26:12 -0300

    Implementacion clase Persona, Pasajero y Piloto. Creacion clase Pasaje. Status - not ready

---
+++ src/main/java/com/gestion_vuelos/exercise_jpa/model/Pasaje.java
@@ -0,0 +1,64 @@
+package com.gestion_vuelos.exercise_jpa.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Pasaje {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "vuelo_pasaje")
+    private Vuelo vuelo;
+
+    @ManyToOne
+    @JoinColumn(name = "pasajero_id")
+    private Pasajero pasajero;
+
+    @OneToOne
+    private Asiento asiento;
+
+    @Column(nullable = false)
+    private double precio;
+
+    public Pasaje(Long id, Vuelo vuelo, Pasajero pasajero, Asiento asiento, double precio) {
+        this.id = id;
+        this.vuelo = vuelo;
+        this.pasajero = pasajero;
+        this.asiento = asiento;
+        this.precio = precio;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Vuelo getVuelo() {
+        return vuelo;
+    }
+
+    public void setVuelo(Vuelo vuelo) {
+        this.vuelo = vuelo;
+    }
+
+    public Pasajero getPasajero() {
+        return pasajero;
+    }
+
+    public void setPasajero(Pasajero pasajero) {
+        this.pasajero = pasajero;
+    }
+}
--- src/main/java/com/gestion_vuelos/exercise_jpa/model/Persona.java
+++ src/main/java/com/gestion_vuelos/exercise_jpa/model/Persona.java
@@ -1,4 +1,60 @@
 package com.gestion_vuelos.exercise_jpa.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // Consultar Single_Table con chicos el Lunes
 public abstract class Persona {
+
+    /* Atributos */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pasaporte_id")
+    private Pasaporte pasaporte;
+
+    protected Persona() {
+    }
+
+    protected Persona(String nombre, Pasaporte pasaporte) {
+        this.nombre = nombre;
+        this.pasaporte = pasaporte;
+    }
+
+    /* Metodos */
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Pasaporte getPasaporte() {
+        return pasaporte;
+    }
+
+    public void setPasaporte(Pasaporte pasaporte) {
+        this.pasaporte = pasaporte;
+    }
+
 }
